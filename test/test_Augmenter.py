91150d97b06d3e79016baabb322fd96f737d3b3f
Pierrick Croissant <pcroissant@ensc.fr>
2021-02-12 13:42:10 +0100

    test: add bert aug

--- test/test_Augmenter.py
+++ test/test_Augmenter.py
@@ -56,15 +56,13 @@ class TestAugmenter(object):
         assert len(augmented_sentence1) == len(sentence)
         assert len(augmented_sentence2) == len(sentence)
     
-    # def test_augment_bert(self):
-    #     aug1 = Augmenter(1)
-    #     aug2 = Augmenter(2)
-    #     sentence = "Sylvanus le roi du NLP"
-    #     augmented_sentence1 = aug1.augment_bert(sentence)
-    #     augmented_sentence2 = aug2.augment_bert(sentence)
+    def test_augment_bert(self):
+        aug1 = Augmenter(1)
+        aug2 = Augmenter(2)
+        sentence = "Sylvanus le roi du NLP"
+        augmented_sentence1 = aug1.augment_bert(sentence)
+        augmented_sentence2 = aug2.augment_bert(sentence)
     
-    #     # assert len(augmented_sentence1) == len(sentence)
-    #     # assert augmented_sentence1 != sentence
-    #     # assert len(augmented_sentence2) == len(sentence)
-    #     # assert augmented_sentence2 != sentence
-    #     # assert augmented_sentence1 != augmented_sentence2
+        assert augmented_sentence1 != sentence
+        assert augmented_sentence2 != sentence
+        assert augmented_sentence1 != augmented_sentence2
